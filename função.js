727e850434d33f268b03fa62ac125b8136eb9fc9
09gdnx90 <127199825+09gdnx90@users.noreply.github.com>
2023-11-10 10:45:03 -0300

    Add files via upload

---
+++ função.js
@@ -0,0 +1,29 @@
+function LeiaSeuNome(autor){
+if(autor == "Samuel"){
+    LeiaSeuNome("Pitbull treinado, doutrinado e posturado")
+}
+}
+function LeiaSeuNome(autor){
+if(autor == "Eduardo A."){
+    LeiaSeuNome("Um grande homem que pega varios homens tambem")
+}
+}
+function LeiaSeuNome(autor){
+if(autor == "Eduarda"){
+    LeiaSeuNome("Se paga de jogadora e perde pra crinça de 8anos Vp")
+}
+}
+function LeiaSeuNome(autor){
+if(autor == "Giuliana"){
+    LeiaSeuNome("Reclama de tudo.")
+}
+}
+
+const autor["Samuel", "Eduardo", "Eduarda" e "Giuliana];
+let contador = 0;
+white[contador < autor,length]{
+    LeiaSeuNome(autor[contador]);
+    contador = contador + 1 ;
+
+    
+}
